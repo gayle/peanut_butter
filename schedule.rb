e2131b8635ccd99eed84e6f2ca3b8f6d50090d93
Gayle <gayle@craigworld.com>
2011-10-20 02:11:05 -0400

    Added another action for stick and puck specifically, and extracted some methods.

--- schedule.rb
+++ schedule.rb
@@ -10,21 +10,42 @@ TIME     = 3
 
 get '/' do
   today = Time.now.strftime("%Y-%m-%d")
-  doc   = Nokogiri::HTML(open('http://www.thechiller.com/rink-schedule'))
-  rows  = doc.css("tr[@class='#{today}']")
+  get_schedule_for today
+end
 
-  return "No events found for #{today}" if rows.empty?
+get '/StickAndPuck' do
+  today = Time.now.strftime("%Y-%m-%d")
+  get_schedule_for today, "stick and puck"
+end
+
+private
+def get_schedule_for(date, event_filter=nil)
+  doc  = Nokogiri::HTML(open('http://www.thechiller.com/rink-schedule'))
+  rows = doc.css("tr[@class='#{date}']")
+
+  return "No events found for #{date}" if rows.empty?
+  return_val = build_xml_for_rows rows, event_filter
+  puts "return value is #{return_val.class} #{return_val.inspect}"
+
+  return_val
+end
+
+def build_xml_for_rows(rows, event_filter)
   builder do |xml|
     xml.instruct! :xml, :version => '1.0'
     xml.schedule do
       rows.each do |row|
-        xml.event do
-          xml.name row.children[NAME].content
-          xml.location row.children[LOCATION].content
-          xml.date row.children[DATE].content
-          xml.time row.children[TIME].content
+        contents = row.children.collect {|c| c.content}
+        if event_filter.nil? or contents.join(" ").match /event_filter/i
+          xml.event do
+            xml.name contents[NAME]
+            xml.location contents[LOCATION]
+            xml.date contents[DATE]
+            xml.time contents[TIME]
+          end
         end
       end
     end
   end
 end
+
--- schedule_test.rb
+++ schedule_test.rb
@@ -16,12 +16,13 @@ class ScheduleTest < Test::Unit::TestCase
 # TODO this test was working when the schedule.rb file was calling "builder :index" to render index.builder view
 # but now that logic was moved out of view, it gets error "Failed assertion, no message given."
 # Figure out a different way to test this response.  Might be an issue with the rack test mock response?
+# For now just call get, b/c the test will at least fail if an exception is thrown.  Still need to add some asserts tho.
 #
-#  def test_it_does_something
-#    get '/'
+  def test_it_does_something
+    get '/'
 #    assert last_response.ok?, "expected 200 response, got #{last_response.status}"
 #    assert !last_response.body.empty?, "response should have some content"
-#  end
+  end
 
   def test_when_no_data_found
     fake_document = flexmock(Nokogiri::HTML::Document)
@@ -31,4 +32,10 @@ class ScheduleTest < Test::Unit::TestCase
     get '/'
     assert_match  /no events found/i, last_response.body
   end
+
+  def test_filters_are_not_case_sensitive
+    get '/StickAndPuck'
+
+  end
 end
+
